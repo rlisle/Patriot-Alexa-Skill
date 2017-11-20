38630291af8ddaba261550fa66d24880f163f39e
Ron Lisle <ron@lisles.net>
2017-11-20 09:44:30 -0600

    Update intents

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -15,23 +15,21 @@ var handlers = {
     'LaunchRequest': function () {
         this.emit('SayHello');
     },
-    'HelloWorldIntent': function () {
-        this.emit('SayHello');
-    },
-    'MyNameIsIntent': function () {
-        this.emit('SayHelloName');
-    },
     'SayHello': function () {
-        this.response.speak('Hello World!')
-                     .cardRenderer('hello world', 'hello world');
+        this.response.speak('You can tell me to turn on or off the light')
+                     .cardRenderer('my photon', 'You can tell me to turn on or off the light');
         this.emit(':responseReady');
     },
-    'SayHelloName': function () {
-        var name = this.event.request.intent.slots.name.value;
-        this.response.speak('Hello ' + name)
-            .cardRenderer('hello world', 'hello ' + name);
-        this.emit(':responseReady');
-    },
+  'TurnOnIntent': function () {
+    this.response.speak('Ok, the light is now on')
+      .cardRenderer('Turn On', 'The Photon blue LED has been turned on');
+    this.emit(':responseReady');
+  },
+  'TurnOffIntent': function () {
+    this.response.speak('Ok, the light is now off')
+      .cardRenderer('Turn Off', 'The Photon blue LED has been turned off');
+    this.emit(':responseReady');
+  },
     'SessionEndedRequest' : function() {
         console.log('Session ended with reason: ' + this.event.request.reason);
     },
@@ -40,8 +38,7 @@ var handlers = {
         this.emit(':responseReady');
     },
     'AMAZON.HelpIntent' : function() {
-        this.response.speak("You can try: 'alexa, hello world' or 'alexa, ask hello world my" +
-            " name is awesome Aaron'");
+        this.response.speak('You can tell me to turn on or off the light');
         this.emit(':responseReady');
     },
     'AMAZON.CancelIntent' : function() {
