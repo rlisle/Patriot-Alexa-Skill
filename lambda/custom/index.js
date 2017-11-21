a9f923d5a46f3f9baec4ea7d6743b9029745969f
Ron Lisle <ron@lisles.net>
2017-11-21 08:03:20 -0600

    Set token

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -3,6 +3,8 @@ const Alexa = require("alexa-sdk");
 const Particle = require('particle-api-js');
 
 const publishName = "patriot";
+//const deviceId = "<your photon device id>";
+var token;
 
 exports.handler = function(event, context) {
     var alexa = Alexa.handler(event, context);
@@ -10,23 +12,34 @@ exports.handler = function(event, context) {
     alexa.execute();
 };
 
+/* Helper methods */
+function log(title, msg) {
+  console.log('**** ' + title + ': ' + JSON.stringify(msg) + '\n');
+}
+
 var handlers = {
     'LaunchRequest': function () {
+      log('Launch', 'Saying hello');
         this.emit('SayHello');
     },
     'SayHello': function () {
+
         this.response.speak('You can tell me to turn on or off the light')
                      .cardRenderer('my photon', 'You can tell me to turn on or off the light');
         this.emit(':responseReady');
     },
   'TurnOnIntent': function () {
+      log('Turn On', 'Turn on intent calling publish');
+      let token = event.session.user.accessToken;
       publish(publishName, "blue:100", token);
     this.response.speak('Ok, the light is now on')
       .cardRenderer('Turn On', 'The Photon blue LED has been turned on');
     this.emit(':responseReady');
   },
   'TurnOffIntent': function () {
-    publish(publishName, "blue:100", token);
+    log('Turn Off', 'Turn off intent calling publish');
+    let token = event.session.user.accessToken;
+    publish(publishName, "blue:0", token);
     this.response.speak('Ok, the light is now off')
       .cardRenderer('Turn Off', 'The Photon blue LED has been turned off');
     this.emit(':responseReady');
